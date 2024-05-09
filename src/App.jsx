b8b0975e73e71e00a5ed0eec9d62c4e376b0f21c
Yash Jain <105738560+yashjaincodder@users.noreply.github.com>
2024-05-09 16:38:29 +0530

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Website from './components/Website'
 import FormFieldEmpty from './components/FormFieldEmpty'
 import Success from './components/Success'
-import Documentation from "./components/Documentation"
+
 const App = () => {
   return (
     <div>
@@ -12,7 +12,6 @@ const App = () => {
         <Route path='/' element={<Website/>}></Route>
         <Route path='/FormField' element={<FormFieldEmpty/>}></Route>
         <Route path='/success' element={<Success/>}></Route>
-        <Route path='/Documentation' element={<Documentation/>}></Route>
       </Routes>
      </BrowserRouter>
     </div>
